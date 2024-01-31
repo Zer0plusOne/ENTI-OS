bcf383cf0b95fe503a31994ab6c0ac9afdb40e03
G.G.S <82466341+Zer0plusOne@users.noreply.github.com>
2024-01-31 14:09:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ void gestionPanel() {
     cout << "Panel de gestion" << endl;
     cout << "\n" << endl;
     cout << "1. Crear usuario" << endl;
-    cout << "2. Modificar password de administrador" << endl;
+    cout << "2. Modificar password de un usuario" << endl;
     cout << "3. Eliminar usuarios" << endl;
     cout << "4. Cerrar sesion" << endl;
 
@@ -36,8 +36,11 @@ void gestionPanel() {
 
     cin >> seleccion;
 
-    string deleteUser; // pese a que no queria declarar las estas variables con tal de no inicializarlas en caso de no ser necesario, las tengo que inicializar aqui porque sino da el siguiente error: transfer of control bypasses initialization of:C/C++(546)
-    string adminPassword;
+    // Bloque de variables de los casos
+
+    string deleteUser; // pese a que no queria declarar las estas variables con tal de no inicializarlas en caso de no ser necesario, las tengo que inicializar aqui porque sino da el siguiente error: transfer of control bypasses initialization of:C/C++(546)// 
+    int modifyPass; // Este sera el valor del indice en el que se encuentran los usuarios en el vector, con tal de mas tarde poder modificar dicho vector
+    string newpass; // Nueva password a insertar
 
     switch (seleccion) {
 
@@ -50,14 +53,18 @@ void gestionPanel() {
         cout << "Ingresa password para el usuario: ";
         cin >> creacionPassword;
 
-        if (creacionPassword == "" || creacionUserName == "") {
+        if (creacionPassword == "" || creacionUserName == "") { // Unicamente verificamos que no haya dejado ninguna de las opciones en blanco
             cout << "Si hombre, se te ha olvidado dar nombre o poner contraseña al usuario, intentalo de nuevo listillo" << endl;
             this_thread::sleep_for(chrono::seconds(1));
+            screenClear();
             gestionPanel();
         }
         else {
-            users.push_back(creacionUserName);
-            passwords.push_back(creacionPassword);
+            users.push_back(creacionUserName); // añadimos el usuario que ha insertado al final del vector "users"
+            passwords.push_back(creacionPassword); // añadimos la password al final del vector "passwords"
+
+            // se insertan al mismo tiempo porque se verifica que el password es el correspondiente al usuario viendo que ambos se encuentren en el mismo numero del indice del vector
+
             cout << "Nuevo usuario aniadido al sistema..." << endl;
             cout << "Volviendo al panel de aministrador..." << endl;
             this_thread::sleep_for(chrono::seconds(2));
@@ -66,28 +73,32 @@ void gestionPanel() {
         }
         break;
     case 2:
-        cout << "Menu de modificacion de password de administrador" << endl;
+        cout << "Menu de modificacion de password" << endl;
         cout << "\n" << endl;
-        cout << "Ingresa la contrasenia actual:" << endl;
-        cin >> adminPassword;
-
-        if (adminPassword != passwords[0]) {
-            cout << "credenciales incorrectas, volviendo al panel de administrador..." << endl;
-            this_thread::sleep_for(chrono::seconds(2));
-            screenClear();
-            gestionPanel();
-
+        cout << "Usuarios Existentes:" << endl;
+        for (int i = 0; i < users.size(); i++) { // recorremos el vector de usuarios de 1 en 1 imprimiendo los valores teniendo en cuenta que "i" siempre sera un numero = o < al tamaño total del vector
+            cout << i << ": " << users[i] << endl;
         }
-        else {
-            cout << "Ingresa la nueva contrasenia:" << endl;
-            cin >> passwords[0];
-            cout << "Contrasenia cambiada con exito" << endl;
-            this_thread::sleep_for(chrono::seconds(2));
-            screenClear();
-            gestionPanel();
+        cout << "Selecciona el usuario al que deseas realizar la modificacion: (numero)";
+        cin >> modifyPass;  // seleccion del usuario al que vamos a modificarle la contraseña
+
+        if (modifyPass == 0) { // Avisaremos en caso que se intente cambiar el password de administrador
+            cout << "Alerta, estas modificando el password del usuario administrador" << endl;
+            cout << "Introduce la nueva contrasenia para ese usuario" << endl;
+            cin >> newpass; // nueva password
+
+            passwords[modifyPass] = newpass; // Para asegurarnos que hemos cambiado el valor correcto, usaremos la seleccion (que recordemos era el valor del usuario en el indice del vector) y lo insertaremos directamente en su espacio correspondiente en el vector passwords
         }
+        else { // Exactamente lo mismo pero sin la necesidad de avisar sobre que se esta intentando cambiar la password del usuario administrador
+            cout << "Introduce la nueva contrasenia para ese usuario" << endl;
+            cin >> newpass;
+
+            passwords[modifyPass] = newpass; // lo mismo de arriba
+        }
+
         break;
     case 3:
+
         cout << "Menu de eliminacion de usuario" << endl;
         cout << "\n" << endl;
         // mostramos todo el vector users
@@ -95,6 +106,7 @@ void gestionPanel() {
         for (const auto& user : users) { // declaramos la constante user con auto para que el compilador pueda detectar el tipo de dato que tiene el vector (gracias GPT por la ayuda en esto y ahorrarme sacar uno a uno los valores del vector)
             cout << user << endl;
         }
+
         cout << "Selecciona el usuario a eliminar: ";
         cin >> deleteUser;
 
@@ -168,27 +180,27 @@ void adminMenu() {
         gestionPanel();
         break;
     case 2:
-        cout << "Usuario:";
-        cin >> user;
-        cout << "Contrasenia:";
-        cin >> password;
+        cout << "Usuario:"; 
+        cin >> user; 
+        cout << "Contrasenia:"; 
+        cin >> password; 
 
         if (user == users[0] && password == passwords[0]) { // en caso de ingresar las credenciales almacenadas en el punto 0 del vector, las cuales corresponden siempre al admin, inicia sesion como admin
-            cout << "Iniciando sesion como administrador" << endl;
-            this_thread::sleep_for(chrono::seconds(2)); // timeout de 2 segundos 
-            screenClear(); // Funcion para limpiar la pantalla
-            adminMenu();
+            cout << "Iniciando sesion como administrador" << endl; 
+            this_thread::sleep_for(chrono::seconds(2)); // timeout de 2 segundos  
+            screenClear(); // Funcion para limpiar la pantalla 
+            adminMenu(); 
         }
         else {
-            for (int i = 1; i < users.size(); i++) { // bucle for que recorre los vectores saltando el primero (indice 0 ya que es el de admin)
-                if (user == users[i] && password == passwords[i]) { // en caso que las credenciales user y password esten almacenadas en la misma posicion, acceso concedido
+            for (int i = 1; i < users.size(); i++) { // bucle for que recorre los vectores saltando el primero (indice 0 ya que es el de admin) 
+                if (user == users[i] && password == passwords[i]) { // en caso que las credenciales user y password esten almacenadas en la misma posicion, acceso concedido  
                     cout << "Iniciando sesion" << endl;
-                    screenClear();
-                    this_thread::sleep_for(chrono::seconds(2));
-                    cout << "ENDPOINT PROVISIONAL" << endl;
+                    screenClear();  
+                    this_thread::sleep_for(chrono::seconds(2)); 
+                    cout << "ENDPOINT PROVISIONAL" << endl; 
                 }
                 else {
-                    cout << "Usuario o contraseña incorrecto" << endl;
+                    cout << "Usuario o contraseña incorrecto" << endl; 
                 }
             }
         }
